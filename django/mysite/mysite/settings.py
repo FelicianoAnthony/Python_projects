dadac1c0098b6deb834cebe9c5c99d541f8012ec
Anthony <afeliciano893@gmail.com>
2017-08-25 20:08:09 -0400

    nokey

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#+dtjdl6c!++0i=)bvoeh0_9s-ry6znkh83g5!s3i698mdo+j6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

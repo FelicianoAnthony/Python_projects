6f23cef9f13a050e2d4b0d951f947b988952501f
Anthony <afeliciano893@gmail.com>
2017-09-20 22:01:18 -0400

    re-arrange

--- django/personal/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-	return render(request, 'personal/home.html')
-
-def contact(request):
-	return render(request, 'personal/basic.html', {'content': ['If you wanna holla, email me at,'
-		'mail@mail.com']})
